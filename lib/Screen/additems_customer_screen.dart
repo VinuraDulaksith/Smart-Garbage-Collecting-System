85bc02290235ff84c5e392e9425fdda066d0526e
VinuraDulaksith <dulaksith@gmail.com>
2023-09-27 09:40:07 +0530

    Smart Garbage System

--- lib/Screen/additems_customer_screen.dart
+++ lib/Screen/additems_customer_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:garbage_collector_1/Screen/SubPages_Customer/Customer_bottles.dart';
 
 import '../utils/colours.dart';
 
@@ -14,23 +15,24 @@ class _AdditemsCustomerScreenState extends State<AdditemsCustomerScreen> {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          gradient: LinearGradient(colors:[
-            hexStringToColor("006400"),
-            hexStringToColor("35ab07"),
-            hexStringToColor("095207")
-          ],
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,)
-        //  colors: [Colors.purple, Colors.blue],
-      ),
+          gradient: LinearGradient(
+        colors: [
+          hexStringToColor("006400"),
+          hexStringToColor("35ab07"),
+          hexStringToColor("095207")
+        ],
+        begin: Alignment.topRight,
+        end: Alignment.bottomLeft,
+      )
+          //  colors: [Colors.purple, Colors.blue],
+          ),
 
       child: Scaffold(
         backgroundColor: Colors.transparent,
         appBar: AppBar(
-          title: Text("Collector_Dashboard"),
+          title: const Text("Add Items"),
           backgroundColor: Colors.transparent,
-          actions: [
-
+          actions: const [
             SizedBox(width: 5),
             // IconButton(
             //   icon: Icon(Icons.logout),
@@ -38,27 +40,147 @@ class _AdditemsCustomerScreenState extends State<AdditemsCustomerScreen> {
             // ),
           ],
         ),
-
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SizedBox(height: 20),
-              FloatingActionButton(
-                backgroundColor: const Color(0xff03dac6),
-                foregroundColor: Colors.black,
-                mini: false,
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+                icon: const Icon(Icons.help_outline_rounded),
+                label: const Text(
+                  "| Bottels and cans",
+                  style: TextStyle(fontSize: 18),
+                ),
                 onPressed: () {
-                  // Respond to button press
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const AddBottles()),
+                  );
                 },
-                child: Icon(Icons.qr_code_scanner),
+
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(20.0),
+                  fixedSize: const Size(220, 60),
+                  textStyle: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                  elevation: 30,
+                  shadowColor: Colors.black,
+                  side: const BorderSide(color: Colors.black, width: 2),
+                  shape: const StadiumBorder(),
+                ),
               ),
-              SizedBox(
+              const SizedBox(
                 width: 200,
                 height: 40,
               ),
-
-
+              ElevatedButton.icon(
+                icon: const Icon(Icons.help_outline_rounded),
+                label: const Text(
+                  "| Plastic and paper",
+                  style: TextStyle(fontSize: 17),
+                ),
+                onPressed: () {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => ShoppingCartScreen()),
+                  //     );
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(20.0),
+                  fixedSize: const Size(220, 60),
+                  textStyle: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                  elevation: 30,
+                  shadowColor: Colors.black,
+                  side: const BorderSide(color: Colors.black, width: 2),
+                  shape: const StadiumBorder(),
+                ),
+              ),
+              const SizedBox(
+                width: 200,
+                height: 40,
+              ),
+              ElevatedButton.icon(
+                icon: const Icon(Icons.help_outline_rounded),
+                label: const Text(
+                  "| Residual waste",
+                  style: TextStyle(fontSize: 18),
+                ),
+                onPressed: () {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => ShoppingCartScreen()),
+                  //     );
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(20.0),
+                  fixedSize: const Size(220, 60),
+                  textStyle: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                  elevation: 30,
+                  shadowColor: Colors.black,
+                  side: const BorderSide(color: Colors.black, width: 2),
+                  shape: const StadiumBorder(),
+                ),
+              ),
+              const SizedBox(
+                width: 200,
+                height: 40,
+              ),
+              ElevatedButton.icon(
+                icon: const Icon(Icons.help_outline_rounded),
+                label: const Text(
+                  "| Electronic",
+                  style: TextStyle(fontSize: 18),
+                ),
+                onPressed: () {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => ShoppingCartScreen()),
+                  //     );
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(20.0),
+                  fixedSize: const Size(220, 60),
+                  textStyle: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                  elevation: 30,
+                  shadowColor: Colors.black,
+                  side: const BorderSide(color: Colors.black, width: 2),
+                  shape: const StadiumBorder(),
+                ),
+              ),
+              const SizedBox(
+                width: 200,
+                height: 40,
+              ),
+              ElevatedButton.icon(
+                icon: const Icon(Icons.help_outline_rounded),
+                label: const Text(
+                  "| Other",
+                  style: TextStyle(fontSize: 18),
+                ),
+                onPressed: () {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => ShoppingCartScreen()),
+                  //     );
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(20.0),
+                  fixedSize: const Size(220, 60),
+                  textStyle: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                  elevation: 30,
+                  shadowColor: Colors.black,
+                  side: const BorderSide(color: Colors.black, width: 2),
+                  shape: const StadiumBorder(),
+                ),
+              ),
+              const SizedBox(
+                width: 200,
+                height: 40,
+              ),
             ],
           ),
         ),
